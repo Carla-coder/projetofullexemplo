7eebf3ac248b55675a0b2d4744da229995e7dc05
Carla-coder <you@example.comcmartezanato@yahoo.com.br>
2024-07-25 11:19:27 -0300

    Att

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
-ECHO est  ativado.
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3000;
+
+const rootes = require('./src/routes');
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use(cors)
